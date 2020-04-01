6f0a0b94857f9d31218bb499246cc85b4e78379d
sophie22 <sophie22@users.noreply.github.com>
2020-04-01 21:19:14 +0100

    change back version numbers

--- src/code.sh
+++ src/code.sh
@@ -7,8 +7,8 @@ set -e -x -o pipefail
 dx-download-all-inputs --parallel
 
 #Extract required resources from assets folder into /home/dnanexus/
-dx cat project-Fjj60Qj4yBGvQXbb5Z6FXkgF:file-Fjp4k3Q4yBGZYpQg3bgpPG95 | tar xf - # ~/hs37d5-fasta.tar -> ~/hs37d5.fa 
-# ~/hs37d5.fa.gz -> ~/hs37d5.fa ~from publicly available project
+dx cat project-Fjj60Qj4yBGvQXbb5Z6FXkgF:file-Fjp4k3Q4yBGZYpQg3bgpPG95 | tar xf - 
+# ~/hs37d5-fasta.tar -> ~/hs37d5.fa ~from 001 Reference > genomes > b37
 dx cat project-Fkb6Gkj433GVVvj73J7x8KbV:file-Fjp4k3j4yBGpK4x73bzbG2P0 | tar xf - 
 # ~/hs37d5-sdf.tar -> ~/hs37d5.sdf/ ~from 001 Reference > genomes > b37
 dx cat project-Fkb6Gkj433GVVvj73J7x8KbV:file-Fjp4k484yBGb6XyZKgy4gjgb | tar xf - 
